b1bd2fd2c5cf039c904b80c488b805085912dc68
quangtruong258 <truonglamasd@gmail.com>
2022-12-25 21:43:58 +0700

    lam do chu de va the loai

--- app/src/main/java/com/example/appnhac/Fragment/Fragment_Playlist.java
+++ app/src/main/java/com/example/appnhac/Fragment/Fragment_Playlist.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.appnhac.Adapter.PlaylistAdapter;
 import com.example.appnhac.Model.Playlist;
 import com.example.appnhac.R;
 import com.example.appnhac.Service.APIService;
@@ -26,17 +29,20 @@ import retrofit2.Response;
 public class Fragment_Playlist extends Fragment {
     View view;
 
-    ListView listView;
-    TextView tenplaylist,xemthemplaylist;
+    ListView listViewplaylist;
+    TextView txttenplaylist,txtxemthemplaylist;
+    PlaylistAdapter playlistAdapter;
+    ArrayList<Playlist> dsplaylist;
+
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_playlist,container,false);
-        listView = view.findViewById(R.id.listviewplaylist);
-        tenplaylist = view.findViewById(R.id.textviewtitleplaylist);
-        xemthemplaylist = view.findViewById(R.id.textviewmoreplaylist);
+        listViewplaylist = view.findViewById(R.id.listviewplaylist);
+        txttenplaylist = view.findViewById(R.id.textviewtitleplaylist);
+        txtxemthemplaylist = view.findViewById(R.id.textviewmoreplaylist);
 
         GetData();
         return view;
@@ -48,7 +54,11 @@ public class Fragment_Playlist extends Fragment {
         callback.enqueue(new Callback<List<Playlist>>() {
             @Override
             public void onResponse(Call<List<Playlist>> call, Response<List<Playlist>> response) {
-                ArrayList<Playlist> dsplaylist = (ArrayList<Playlist>) response.body();
+                dsplaylist = (ArrayList<Playlist>) response.body();
+                playlistAdapter = new PlaylistAdapter(getActivity(), android.R.layout.simple_list_item_1,dsplaylist);
+                listViewplaylist.setAdapter(playlistAdapter);
+                setListViewHeightBasedOnChildren(listViewplaylist);
+
             }
 
             @Override
@@ -57,5 +67,32 @@ public class Fragment_Playlist extends Fragment {
             }
         });
 
+    }
+
+    public void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = listView.getPaddingTop() + listView.getPaddingBottom();
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.AT_MOST);
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+
+            if(listItem != null){
+                // This next line is needed before you call measure or else you won't get measured height at all. The listitem needs to be drawn first to know the height.
+                listItem.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+                listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+                totalHeight += listItem.getMeasuredHeight();
+
+            }
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 }
